4764556710f4b57c48aba221dec4ff62f36aff3a
peacekeeper <markus@danubetech.com>
2017-05-08 09:00:33 +0200

    WIP test

--- src/test/java/com/danubetech/libsovrin/LedgerTest.java
+++ src/test/java/com/danubetech/libsovrin/LedgerTest.java
@@ -7,10 +7,10 @@ import org.junit.Assert;
 
 import com.danubetech.libsovrin.ledger.Ledger;
 import com.danubetech.libsovrin.ledger.Ledger.BuildGetDdoRequestResult;
+import com.danubetech.libsovrin.ledger.Ledger.BuildGetNymRequestResult;
 import com.danubetech.libsovrin.ledger.LedgerImpl;
 import com.danubetech.libsovrin.pool.Pool;
 import com.danubetech.libsovrin.pool.PoolImpl;
-import com.danubetech.libsovrin.pool.Pool.CreatePoolLedgerConfigOptions;
 
 import junit.framework.TestCase;
 
@@ -43,6 +43,12 @@ public class LedgerTest extends TestCase {
 		BuildGetDdoRequestResult result1 = future1.get();
 		Assert.assertNotNull(result1);
 		String requestJson1 = result1.getRequestJson();
+		Assert.assertNotNull(requestJson1);
+
+		Future<BuildGetNymRequestResult> future2 = ledger.buildGetNymRequest("did:sov:21tDAKCERh95uGgKbJNHYp", "did:sov:1yvXbmgPoUm4dl66D7KhyD");
+		BuildGetNymRequestResult result2 = future2.get();
+		Assert.assertNotNull(result2);
+		String requestJson2 = result2.getRequestJson();
 		Assert.assertNotNull(requestJson1);
 	}
 }
