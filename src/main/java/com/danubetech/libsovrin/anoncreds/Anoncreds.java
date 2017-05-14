60ea1ddfe7ea37c3d374c09335136955e0cc033e
peacekeeper <markus@danubetech.com>
2017-05-14 08:59:59 +0200

    WIP anoncreds

--- src/main/java/com/danubetech/libsovrin/anoncreds/Anoncreds.java
+++ src/main/java/com/danubetech/libsovrin/anoncreds/Anoncreds.java
@@ -5,13 +5,7 @@ import java.util.concurrent.Future;
 
 import com.danubetech.libsovrin.LibSovrin;
 import com.danubetech.libsovrin.SovrinException;
-import com.danubetech.libsovrin.signus.SignusResults.CreateAndStoreMyDidResult;
-import com.danubetech.libsovrin.signus.SignusResults.DecryptResult;
-import com.danubetech.libsovrin.signus.SignusResults.EncryptResult;
-import com.danubetech.libsovrin.signus.SignusResults.ReplaceKeysResult;
-import com.danubetech.libsovrin.signus.SignusResults.SignResult;
-import com.danubetech.libsovrin.signus.SignusResults.StoreTheirDidResult;
-import com.danubetech.libsovrin.signus.SignusResults.VerifySignatureResult;
+import com.danubetech.libsovrin.anoncreds.AnoncredsResults.IssuerCreateAndStoreClaimDefResult;
 import com.danubetech.libsovrin.wallet.Wallet;
 import com.sun.jna.Callback;
 
@@ -28,228 +22,34 @@ public class Anoncreds extends LibSovrin.APIJava {
 	 * STATIC METHODS
 	 */
 
-	public static Future<CreateAndStoreMyDidResult> createAndStoreMyDid(
+	public static Future<IssuerCreateAndStoreClaimDefResult> issuerCreateAndStoreClaimDef(
 			Wallet wallet,
-			String didJson) throws SovrinException {
+			String schemaJson, 
+			String signatureType, 
+			boolean createNonRevoc) throws SovrinException {
 
-		final CompletableFuture<CreateAndStoreMyDidResult> future = new CompletableFuture<> ();
+		final CompletableFuture<IssuerCreateAndStoreClaimDefResult> future = new CompletableFuture<> ();
 
 		Callback callback = new Callback() {
 
 			@SuppressWarnings("unused")
-			public void callback(int xcommand_handle, int err, String did, String verkey, String pk) {
+			public void callback(int xcommand_handle, int err, String claim_def_json, String claim_def_uuid) {
 
 				if (! checkCallback(future, xcommand_handle, err)) return;
 
-				CreateAndStoreMyDidResult result = new CreateAndStoreMyDidResult(did, verkey, pk);
+				IssuerCreateAndStoreClaimDefResult result = new IssuerCreateAndStoreClaimDefResult(claim_def_json, claim_def_uuid);
 				future.complete(result);
 			}
 		};
 
 		int walletHandle = wallet.getWalletHandle();
 
-		int result = LibSovrin.api.sovrin_create_and_store_my_did(
+		int result = LibSovrin.api.sovrin_issuer_create_and_store_claim_def(
 				FIXED_COMMAND_HANDLE, 
 				walletHandle, 
-				didJson,
-				callback);
-
-		checkResult(result);
-
-		return future;
-	}
-
-	public static Future<ReplaceKeysResult> replaceKeys(
-			Wallet wallet,
-			String did,
-			String identityJson) throws SovrinException {
-
-		final CompletableFuture<ReplaceKeysResult> future = new CompletableFuture<> ();
-
-		Callback callback = new Callback() {
-
-			@SuppressWarnings("unused")
-			public void callback(int xcommand_handle, int err, String verkey, String pk) {
-
-				if (! checkCallback(future, xcommand_handle, err)) return;
-
-				ReplaceKeysResult result = new ReplaceKeysResult(verkey, pk);
-				future.complete(result);
-			}
-		};
-
-		int walletHandle = wallet.getWalletHandle();
-
-		int result = LibSovrin.api.sovrin_replace_keys(
-				FIXED_COMMAND_HANDLE, 
-				walletHandle, 
-				did,
-				identityJson,
-				callback);
-
-		checkResult(result);
-
-		return future;
-	}
-
-	public static Future<StoreTheirDidResult> storeTheirDid(
-			Wallet wallet,
-			String identityJson) throws SovrinException {
-
-		final CompletableFuture<StoreTheirDidResult> future = new CompletableFuture<> ();
-
-		Callback callback = new Callback() {
-
-			@SuppressWarnings("unused")
-			public void callback(int xcommand_handle, int err) {
-
-				if (! checkCallback(future, xcommand_handle, err)) return;
-
-				StoreTheirDidResult result = new StoreTheirDidResult();
-				future.complete(result);
-			}
-		};
-
-		int walletHandle = wallet.getWalletHandle();
-
-		int result = LibSovrin.api.sovrin_store_their_did(
-				FIXED_COMMAND_HANDLE, 
-				walletHandle, 
-				identityJson,
-				callback);
-
-		checkResult(result);
-
-		return future;
-	}
-
-	public static Future<SignResult> sign(
-			Wallet wallet,
-			String did,
-			String msg) throws SovrinException {
-
-		final CompletableFuture<SignResult> future = new CompletableFuture<> ();
-
-		Callback callback = new Callback() {
-
-			@SuppressWarnings("unused")
-			public void callback(int xcommand_handle, int err, String signature) {
-
-				if (! checkCallback(future, xcommand_handle, err)) return;
-
-				SignResult result = new SignResult(signature);
-				future.complete(result);
-			}
-		};
-
-		int walletHandle = wallet.getWalletHandle();
-
-		int result = LibSovrin.api.sovrin_sign(
-				FIXED_COMMAND_HANDLE, 
-				walletHandle, 
-				did,
-				msg,
-				callback);
-
-		checkResult(result);
-
-		return future;
-	}
-
-	public static Future<VerifySignatureResult> verifySignature(
-			Wallet wallet,
-			String did,
-			String msg,
-			String signature) throws SovrinException {
-
-		final CompletableFuture<VerifySignatureResult> future = new CompletableFuture<> ();
-
-		Callback callback = new Callback() {
-
-			@SuppressWarnings("unused")
-			public void callback(int xcommand_handle, int err, boolean valid) {
-
-				if (! checkCallback(future, xcommand_handle, err)) return;
-
-				VerifySignatureResult result = new VerifySignatureResult(valid);
-				future.complete(result);
-			}
-		};
-
-		int walletHandle = wallet.getWalletHandle();
-
-		int result = LibSovrin.api.sovrin_verify_signature(
-				FIXED_COMMAND_HANDLE, 
-				walletHandle, 
-				did,
-				msg,
-				signature,
-				callback);
-
-		checkResult(result);
-
-		return future;
-	}
-
-	public static Future<EncryptResult> encrypt(
-			Wallet wallet,
-			String did,
-			String msg) throws SovrinException {
-
-		final CompletableFuture<EncryptResult> future = new CompletableFuture<> ();
-
-		Callback callback = new Callback() {
-
-			@SuppressWarnings("unused")
-			public void callback(int xcommand_handle, int err, String encryptedMsg) {
-
-				if (! checkCallback(future, xcommand_handle, err)) return;
-
-				EncryptResult result = new EncryptResult(encryptedMsg);
-				future.complete(result);
-			}
-		};
-
-		int walletHandle = wallet.getWalletHandle();
-
-		int result = LibSovrin.api.sovrin_encrypt(
-				FIXED_COMMAND_HANDLE, 
-				walletHandle, 
-				did,
-				msg,
-				callback);
-
-		checkResult(result);
-
-		return future;
-	}
-
-	public static Future<DecryptResult> decrypt(
-			Wallet wallet,
-			String did,
-			String encryptedMsg) throws SovrinException {
-
-		final CompletableFuture<DecryptResult> future = new CompletableFuture<> ();
-
-		Callback callback = new Callback() {
-
-			@SuppressWarnings("unused")
-			public void callback(int xcommand_handle, int err, String decryptedMsg) {
-
-				if (! checkCallback(future, xcommand_handle, err)) return;
-
-				DecryptResult result = new DecryptResult(decryptedMsg);
-				future.complete(result);
-			}
-		};
-
-		int walletHandle = wallet.getWalletHandle();
-
-		int result = LibSovrin.api.sovrin_decrypt(
-				FIXED_COMMAND_HANDLE, 
-				walletHandle, 
-				did,
-				encryptedMsg,
+				schemaJson,
+				signatureType,
+				createNonRevoc,
 				callback);
 
 		checkResult(result);
