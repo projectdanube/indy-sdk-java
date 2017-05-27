7769ae404c44a4bdbf1acd1ef91d25b80dc97eff
Markus Sabadello <markus@danubetech.com>
2017-05-27 10:03:20 +0200

    report native error code

--- src/main/java/com/danubetech/libsovrin/SovrinException.java
+++ src/main/java/com/danubetech/libsovrin/SovrinException.java
@@ -9,8 +9,8 @@ public class SovrinException extends Exception {
 		super(message);
 	}
 
-	public static SovrinException fromErrorCode(ErrorCode errorCode) {
+	public static SovrinException fromErrorCode(ErrorCode errorCode, int err) {
 
-		return new SovrinException("" + (errorCode == null ? null : errorCode.name()) + ": " + (errorCode == null ? null : errorCode.value()));
+		return new SovrinException("" + (errorCode == null ? null : errorCode.name()) + ": " + (errorCode == null ? null : errorCode.value()) + " (" + Integer.toString(err) + ")");
 	}
 }
--- src/main/java/com/danubetech/libsovrin/SovrinJava.java
+++ src/main/java/com/danubetech/libsovrin/SovrinJava.java
@@ -29,7 +29,7 @@ public class SovrinJava {
 			assert(xcommand_handle == FIXED_COMMAND_HANDLE);
 
 			ErrorCode errorCode = ErrorCode.valueOf(err);
-			if (! ErrorCode.Success.equals(errorCode)) { future.completeExceptionally(SovrinException.fromErrorCode(errorCode)); return false; }
+			if (! ErrorCode.Success.equals(errorCode)) { future.completeExceptionally(SovrinException.fromErrorCode(errorCode, err)); return false; }
 
 			return true;
 		}
@@ -37,7 +37,7 @@ public class SovrinJava {
 		protected static void checkResult(int result) throws SovrinException {
 
 			ErrorCode errorCode = ErrorCode.valueOf(result);
-			if (! ErrorCode.Success.equals(errorCode)) throw SovrinException.fromErrorCode(errorCode);
+			if (! ErrorCode.Success.equals(errorCode)) throw SovrinException.fromErrorCode(errorCode, result);
 		}
 
 		@Override
