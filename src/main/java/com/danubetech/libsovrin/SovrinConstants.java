0a5b601dbc45e59283a8dec035b8452608720644
Markus Sabadello <markus@danubetech.com>
2017-05-29 06:47:35 +0200

    adding Sovrin constants

---
+++ src/main/java/com/danubetech/libsovrin/SovrinConstants.java
@@ -0,0 +1,17 @@
+package com.danubetech.libsovrin;
+
+public class SovrinConstants {
+
+	public static final String ROLE_TRUSTEE = "0";
+	public static final String ROLE_STEWARD = "2";
+
+	public static final String OP_NODE = "0";
+	public static final String OP_NYM = "1";
+	public static final String OP_ATTRIB = "100";
+	public static final String OP_SCHEMA = "101";
+	public static final String OP_CLAIM_DEF = "102";
+	public static final String OP_GET_ATTR = "104";
+	public static final String OP_GET_NYM = "105";
+	public static final String OP_GET_SCHEMA = "107";
+	public static final String OP_GET_CLAIM_DEF = "108";
+}
