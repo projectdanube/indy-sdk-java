1739e06bff46033f809726676097281028b1ac60
peacekeeper <markus@danubetech.com>
2017-06-06 09:21:39 +0200

    update error codes

--- src/main/java/com/danubetech/libsovrin/ErrorCode.java
+++ src/main/java/com/danubetech/libsovrin/ErrorCode.java
@@ -39,7 +39,11 @@ public enum ErrorCode {
     // Invalid library state was detected in runtime. It signals library bug
     CommonInvalidState(109),
 
+    // Object (json, config, key, claim and etc...) passed by library caller has invalid structure
     CommonInvalidStructure(110),
+
+    // IO Error
+    CommonIOError(111),
 
     // Wallet errors
     // Caller passed invalid wallet handle
@@ -57,70 +61,46 @@ public enum ErrorCode {
     // Requested entity id isn't present in wallet
     WalletNotFoundError(204),
 
-    // Wallet files referenced in open_wallet have invalid data format
-    WalletInvalidDataFormat(205),
-
-    // IO error during access wallet backend
-    WalletIOError(206),
-
     // Trying to use wallet with pool that has different name
-    WalletIncompatiblePoolError(207),
-
-    // Trying to open wallet with invalid configuration
-    WalletInvalidConfiguration(208),
-
-    // Error in wallet backend
-    WalletBackendError(209),
+    WalletIncompatiblePoolError(205),
 
     // Ledger errors
     // Trying to open pool ledger that wasn't created before
     PoolLedgerNotCreatedError(300),
 
-    // Invalid pool ledger configuration was passed to open_pool_ledger or create_pool_ledger
-    PoolLedgerInvalidConfiguration(301),
-
-    // Pool ledger files referenced in open_pool_ledger have invalid data format
-    PoolLedgerInvalidDataFormat(302),
-
     // Caller passed invalid pool ledger handle
-    PoolLedgerInvalidPoolHandle(303),
+    PoolLedgerInvalidPoolHandle(301),
 
-    // IO error during access pool ledger files
-    PoolLedgerIOError(304),
+    // Pool ledger terminated
+    PoolLedgerTerminated(302),
 
     // No concensus during ledger operation
-    LedgerNoConsensusError(305),
+    LedgerNoConsensusError(303),
 
     // Attempt to send unknown or incomplete transaction message
-    LedgerInvalidTransaction(306),
+    LedgerInvalidTransaction(304),
 
     // Attempt to send transaction without the necessary privileges
-    LedgerSecurityError(307),
-
-    // IO error during sending of ledger transactions or catchup process
-    LedgerIOError(308),
+    LedgerSecurityError(305),
 
     // Crypto errors
-    // Invalid structure of any crypto promitives (keys, signatures, seeds and etc...)
-    CryptoInvalidStructure(400),
-
-    // Unknown crypto type was requested for signing/verifiyng or encoding/decoding
-    CryptoUnknownTypeError(401),
-
     // Revocation registry is full and creation of new registry is necessary
-    CryptoRevocationRegistryFullError(402),
+    AnoncredsRevocationRegistryFullError(400),
 
-    CryptoInvalidUserRevocIndex(403),
+    AnoncredsInvalidUserRevocIndex(401),
 
-    // Error in crypto backend
-    CryptoBackendError(404),
+    AnoncredsAccumulatorIsFull(402),
 
-    AnoncredsNotIssuedError(405),
+    AnoncredsNotIssuedError(403),
 
     // Attempt to generate master secret with dupplicated name
-    AnoncredsMasterSecretDuplicateNameError(406),
+    AnoncredsMasterSecretDuplicateNameError(404),
 
-    ProofRejected(407);
+    AnoncredsProofRejected(405),
+
+    // Signus errors
+    // Unknown format of DID entity keys
+    SignusUnknownCryptoError(500);
 
 	private int value;
 	private static Map<Integer, ErrorCode> map = new HashMap<> ();
